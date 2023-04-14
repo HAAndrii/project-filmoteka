8da746c4af85f149d29d9d5ed464f734ffaff334
Dinis PC <DinisZabrodsky@gmail.com>
2023-04-14 09:53:18 +0300

    Закидай лише це оновлення

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
 import {modal} from "./js/modal";
 
 modal();
+=======
+import header from './js/header';
+
+import {modal} from "./js/modal";
+
+modal();
+>>>>>>> Stashed changes
