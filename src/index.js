7ba22ac8933fd0a3df4190cdf322e07117103e6c
Andrii Kharchenko <107865478+HAAndrii@users.noreply.github.com>
2023-04-15 11:52:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream
-import {modal} from "./js/modal";
 
-modal();
-=======
 import header from './js/header';
 
 import {modal} from "./js/modal";
 
 modal();
->>>>>>> Stashed changes
