693f185c9d2d22143730a400ea172a403de38125
Gwinblade740 <61421307+Gwinblade740@users.noreply.github.com>
2023-04-17 12:14:10 +0300

    renderLocalSt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import header from './js/header';
 import getTrending from './js/getTrending';
 import { modal } from './js/modal';
+import modalLocalStr from './js/modalLocalStr';
+import renderLocalStr from './js/renderLocalStr';
 modal();
