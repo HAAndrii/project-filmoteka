6cb9015ab6f0f393dfc211aeb6f9481a2f7a2a9d
Andrii Kharchenko <107865478+HAAndrii@users.noreply.github.com>
2023-04-18 20:35:13 +0300

    add fix ls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import header from './js/header';
 import getTrending from './js/getTrending';
 import { modal } from './js/modal';
-import modalLocalStr from './js/modalLocalStr';
+//import modalLocalStr from './js/modalLocalStr';
 import renderLocalStr from './js/renderLocalStr';
 import modalMovie from './js/modalMovie';
 modal();
